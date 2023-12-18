bb51d399f3e0669260debbb54cd588f4999b3368
Prasanthy <prasanthybhaskaran@gmail.com>
2023-12-18 16:28:49 +1300

    commit

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,34 @@
 const mongoose = require('mongoose');
+
 const ProductSchema = new mongoose.Schema({
-    image: {
+    property_pic: {
         type: String,
         required: [true, 'Please enter a title']
     },
-    name: [{
+    pet: {
         type: String,
         required: [true, 'Please enter a title']
-    }],
-    color: [{
+    },
+    name: {
         type: String,
         required: [true, 'Please enter a color']
-    }],
-
-    amount: {
-        type: Number,
-        required: [true, 'Please enter a price']
-    }
-    
+    },
+    level: {
+        type: String,
+        required: [true, 'Please enter a color']
+    },
+    bedroom: {
+        type: String,
+        required: [true, 'Please enter a color']
+    },
+    bathroom: {
+        type: String,
+        required: [true, 'Please enter a color']
+    },
+    propertytype: {
+        type: String,
+        required: [true, 'Please enter a color']
+    }    
 });
 
 const Product = mongoose.model('Product', ProductSchema);
